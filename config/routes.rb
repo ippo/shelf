0ff0609bb64cab00ed1e0abc434df01b0a17f6d3
ippo <ippo124C41plus@gmail.com>
2017-04-21 17:06:52 +0900

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
-  resources :articles do
-    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
